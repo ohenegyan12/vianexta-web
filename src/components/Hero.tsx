837234f5f3e8566b0ae6ea5f09c8b0fe5f76f59f
ohene gyan <ohenegyan159@gmail.com>
2025-12-07 15:50:29 +0000

    new

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -120,7 +120,7 @@ function Hero({ onBuyClick, onBuildClick }: HeroProps) {
   }, [activeTab])
 
   return (
-    <section className={`${activeTab === 'buy' ? 'bg-[#09543D]' : 'bg-[#F9F7F1]'} py-8 lg:py-20 relative overflow-hidden transition-colors duration-300`}>
+    <section className={`${activeTab === 'buy' ? 'bg-[#09543D]' : 'bg-[#F9F7F1]'} mt-[65px] py-8 lg:mt-[73px] lg:py-20 relative overflow-hidden transition-colors duration-300`}>
       {/* Left dashed line - Desktop only */}
       <div className="absolute left-0 top-1/2 -translate-y-1/2 -translate-y-28 translate-x-64 hidden lg:block" style={{ zIndex: 1 }}>
         <img ref={leftDashesRef} src={dashesLeft} alt="Left dashed line" className="h-auto w-60" />
